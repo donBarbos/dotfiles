159874d48981f5d8e04fff22586525c593922fb7
DONSIMON92 <semenische@yandex.com>
2022-01-21 10:15:12 +0300

    delete extra files

--- .config/i3/change_i3config_default.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sed -i 's/bindsym ctrl+o kill/bindsym ctrl+q kill/' $HOME/.config/i3/config
-sed -i 's/bindsym Mod1+space exec rofi -show run/bindsym ctrl+space exec rofi -show run/' $HOME/.config/i3/config
