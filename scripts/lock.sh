e7361431fdfb05bf75f3745faf2a6e71a9bff263
donBarbos <donbarbos@proton.me>
2023-11-05 12:40:13 +0400

    update configs

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 
-PICTURE=/tmp/lock.png
-BLUR="0x8"  # radiusxsigma
+GRACE=$1
 
-grim -g "0,0 1505x847" $PICTURE
-convert $PICTURE -blur $BLUR $PICTURE
-swaylock -i $PICTURE
-rm $PICTURE
+swaylock \
+	--screenshots \
+	--clock \
+	--indicator \
+	--indicator-radius 70 \
+	--indicator-thickness 7 \
+	--effect-blur 7x5 \
+	--effect-vignette 0.5:0.5 \
+	--ring-color bb00cc \
+	--key-hl-color 00ff00 \
+	--line-color 00000000 \
+	--inside-color 00000088 \
+	--separator-color 00000000 \
+	--grace $GRACE \
+	--fade-in 0.2
+#PICTURE=/tmp/lock.png
+#BLUR="0x8"
+
+#grim -g "0,0 1505x847" $PICTURE
+#convert $PICTURE -blur $BLUR $PICTURE
+#swaylock -i $PICTURE
+#rm $PICTURE
--- zshrc
+++ zshrc
@@ -70,3 +70,21 @@ export MANPATH="${MANPATH-$(manpath)}:$NPM_PACKAGES/share/man"
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+export PATH="$PATH:$HOME/.foundry/bin"
+
+export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
+
+# pnpm
+export PNPM_HOME="$HOME/.local/share/pnpm"
+case ":$PATH:" in
+  *":$PNPM_HOME:"*) ;;
+  *) export PATH="$PNPM_HOME:$PATH" ;;
+esac
+# pnpm end
+
+# ngrok
+if command -v ngrok &>/dev/null; then
+  eval "$(ngrok completion)"
+fi
+# ngrok end
